f1920e3a7886632ae81cf7a9ef96e641ebfd6d4f
Yuliy92 <123620474+Yuliy92@users.noreply.github.com>
2024-07-04 16:07:42 +0700

    Add files via upload

---
+++ _test_/index.11.6.4 (HW-01).js
@@ -0,0 +1,4 @@
+function getPercents(percent, number) {
+  const result = number * (percent / 100);
+  return result;
+}
---
+++ _test_/index.test.11.6.4 (HW-01).js
@@ -0,0 +1,5 @@
+const getPercents = require("./index.11.6.4 (HW-01).js");
+
+test("30 percent of 200 is 60", () => {
+  expect(getPercents(30, 200)).toBe(60);
+});
